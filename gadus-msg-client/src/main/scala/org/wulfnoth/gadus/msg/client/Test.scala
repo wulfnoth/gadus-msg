e77e3b9c3a6c4aabe253fd665f719974049e6bc9
wulfnoth <congj.yang@hotmail.com>
2017-09-21 15:35:19 +0800

    init

---
+++ gadus-msg-client/src/main/scala/org/wulfnoth/gadus/msg/client/Test.scala
@@ -0,0 +1,8 @@
+package org.wulfnoth.gadus.msg.client
+
+/**
+  * Created by cloud on 2017/9/21.
+  */
+class Test {
+
+}
---
+++ gadus-msg-common/src/main/scala/org/wulfnoth/gadus/msg/common/Test.scala
@@ -0,0 +1,8 @@
+package org.wulfnoth.gadus.msg.common
+
+/**
+  * Created by cloud on 2017/9/21.
+  */
+class Test {
+
+}
